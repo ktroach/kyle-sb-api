471ccad532293d6d70dff05feaf735bf00f87ee5
ktroach <kylroa@msn.com>
2021-09-09 18:17:37 -0500

    im too good

--- src/main/java/net/guides/springboot2/crud/controller/HomeController.java
+++ src/main/java/net/guides/springboot2/crud/controller/HomeController.java
@@ -4,10 +4,7 @@ package net.guides.springboot2.crud.controller;
 import net.guides.springboot2.crud.model.Home;
 import net.guides.springboot2.crud.repository.HomeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -18,6 +15,12 @@ public class HomeController {
 
     @Autowired
     private HomeRepository homeRepository;
+
+    @CrossOrigin
+    @PostMapping("/homes")
+    public Home createHome(@Valid @RequestBody Home home) {
+        return homeRepository.save(home);
+    }
 
     @CrossOrigin
     @GetMapping("/homes")
